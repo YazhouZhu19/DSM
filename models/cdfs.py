7b375c0828c2da3b6ffdf84d4142dd9b5fe87b36
Yazhou Zhu <132653569+YazhouZhu19@users.noreply.github.com>
2024-12-23 02:13:32 +0800

    Update cdfs.py

--- models/cdfs.py
+++ models/cdfs.py
@@ -25,7 +25,6 @@ import torchvision.models as models
 from .base.feature import extract_feat_vgg, extract_feat_res
 from .base.correlation import Correlation
 from .learner import MixedLearner
-# from .learner import Matching
 import matplotlib.pyplot as plt
 
 from mamba_ssm import Mamba 
